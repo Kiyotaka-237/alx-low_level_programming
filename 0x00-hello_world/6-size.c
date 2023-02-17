a45fd780fbd6af6e90c409f299cdeb13711ed91a
Kiyotaka-237 <draculemihawk232@gmail.com>
2023-02-16 23:42:26 -0800

    betty

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,8 +13,8 @@ int main(void)
 
 	printf("Size of a char: %zu byte(s)\n", sizeof(me));
 	printf("Size of an int: %zu byte(s)\n", sizeof(you));
-	printf("size of a long int: %zu byte(s)\n", sizeof(we));
-	printf("size of a long long int: %zu bytes(s)\n", sizeof(us));
-	printf("size of a float: %zu byte(s)\n", sizeof(they));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(we));
+	printf("Size of a long long int: %zu bytes(s)\n", sizeof(us));
+	printf("Size of a float: %zu byte(s)\n", sizeof(they));
 	return (0);
 }
