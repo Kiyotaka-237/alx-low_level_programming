a3e722db04733e5d21bd0eaacbae78d849b4db0b
Kiyotaka-237 <draculemihawk232@gmail.com>
2023-02-17 21:52:00 -0800

    betty

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -12,12 +12,17 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n < 0)
+	{
 		printf("%d in negative\n", n);
-	else
+	}
 	if (n > 0)
+	{
 		printf("%d is positive\n", n);
+	}
 	else
 	if (n == 0)
+	{
 		printf("5d is zero\n", n);
+	}
 	return (0);
 }
