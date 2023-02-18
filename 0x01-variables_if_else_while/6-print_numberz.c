584eb47490c265e4fe6fb78dead32a38ec626884
Kiyotaka-237 <draculemihawk232@gmail.com>
2023-02-18 13:54:54 -0800

    betty

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Entry
+ * Return: Always 0
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+		putchar(n + '0');
+	}
+	putchar('\n');
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry
+ * Return: Always 0
+ */
+int main(void)
+{
+	char ch, n = '0';
+
+	while (n <= '0')
+	{
+		putchar(n++);
+	}
+	for (ch = 'a'; ch <= 'f; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
