ed1de06ee3bd30b68d5c60066e13b7001abf6cbb
Kiyotaka-237 <draculemihawk232@gmail.com>
2023-02-23 00:14:31 -0800

    betty

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - function that prints 10x alphabet
+ *
+ * Return: Always 0
+ */
+
+void print_alphabet_x10(void)
+{
+	int i;
+	char ch;
+
+	for (i = 1; i <= 10; i++)
+	{
+		for (ch = 'a'; ch <= 'z'; ch++)
+		{
+			_putchar(ch);
+		}
+		_putchar('\n');
+	}
+}
