ca1cb1d7360b248f8119bb98f228d70d2c6efe52
Kiyotaka-237 <draculemihawk232@gmail.com>
2023-02-23 02:35:57 -0800

    alx

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * times_table - times table
+ *
+ * Return: Always 0
+ */
+
+void times_table(void)
+{
+	int n, i;
+
+	for (n = 0; n <= 9; n++)
+	{
+		for (i = 0; i <= 9; i++)
+		{
+			printf("%d, ", i * n);
+			if (i == 9)
+				prinf("%d", i * n);
+		}
+		printf("\n");
+	}
+}
