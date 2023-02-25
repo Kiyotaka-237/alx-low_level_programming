2f8e394b92ade6bfb1aee2319c139cb9c0dcb844
Kiyotaka-237 <draculemihawk232@gmail.com>
2023-02-24 16:07:47 -0800

    alx

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two numberss
+ *
+ * @a: function parameter
+ * @b: function parameter
+ *
+ * Return: product
+ */
+int mul(int a, int b)
+{
+	int product;
+
+	product = (a * b);
+	return (product);
+}
