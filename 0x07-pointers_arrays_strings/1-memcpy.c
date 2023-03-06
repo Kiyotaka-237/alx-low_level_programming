152648a7c7db2f0941819bf623971b244db33e38
Kiyotaka-237 <draculemihawk232@gmail.com>
2023-03-06 13:21:00 -0800

    alx

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _memcpy - what ever
+ *
+ * @dest: who cares
+ * @src: no one
+ * @n: yh no one
+ *
+ * Return: dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	int i = 0;
+
+	while (i < n)
+	{
+		dest++ = src++
+	}
+	return (dest);
+}
