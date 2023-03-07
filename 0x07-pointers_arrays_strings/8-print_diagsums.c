f5d7149fdae85ec8cc89afeff09a4104700f3e37
Kiyotaka-237 <draculemihawk232@gmail.com>
2023-03-07 14:53:24 -0800

    alx

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_diagsums - what ever
+ *
+ * @a: array
+ * @size: bla bla
+ *
+ * Return: always 0
+ */
+void print_diagsums(int *a, int size)
+{
+	int i, sum_diag_1 = 0, sum_diag_2 = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		sum_diag_1 += a[(i * size) + i];
+		sum_diag_2 += a[(size - 1) (1 + i)];
+	}
+	printf("%d, %d\n", sum_diag_1, sum_diag_2);
+}
+
