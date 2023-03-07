aa82cf180f403001d6440e4aea4abd95f6779751
Kiyotaka-237 <draculemihawk232@gmail.com>
2023-03-07 15:09:41 -0800

    alx

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -16,7 +16,7 @@ void print_diagsums(int *a, int size)
 	for (i = 0; i < size; i++)
 	{
 		sum_diag_1 += a[(i * size) + i];
-		sum_diag_2 += a[(size - 1) + ((size - 1) * i];
+		sum_diag_2 += a[(size - 1) + ((size - 1) * i)];
 	}
 	printf("%d, %d\n", sum_diag_1, sum_diag_2);
 }
