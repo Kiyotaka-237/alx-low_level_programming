74f55fe5451b21985aeba410e01630147c782416
Kiyotaka-237 <draculemihawk232@gmail.com>
2023-03-08 16:21:11 -0800

    alx

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/** 
+ * _puts_recursion - what ever
+ *
+ * @s: shdei
+ *
+ * Return: void
+ */
+void _puts_recursion(char *s)
+{
+	if (*s)
+	{
+		_putchar(*s);
+		_puts_recursion(++s);
+	}
+	else
+	{
+		_putchar('\n");
+	}
+}
