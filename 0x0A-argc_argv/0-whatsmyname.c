6b136a66b302cd63b074cf78c2581b97ae3857de
Kiyotaka-237 <draculemihawk232@gmail.com>
2023-03-10 10:27:23 -0800

    alx

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - kdjlkd
+ *
+ * @argc: dlkj
+ * @argv: sdlkj
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[argc * 0]);
+	return (0);
+}
