3d2da080da47c4ffc17993215d98865bea4e1541
Kiyotaka-237 <draculemihawk232@gmail.com>
2023-03-10 10:35:05 -0800

    alx

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - djk
+ *
+ * @argc: akjl
+ * @argv: dkdkd
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[]__attribute__((unused)))
+{
+	pritnf("%d\n", argc - 1);
+	return (0);
+}
